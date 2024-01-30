ab3267bf6d7922ab1716ab449b315ae0c2be39f4
Oma-yma1 <zgani.omayma@gmail.com>
2024-01-30 18:08:11 +0000

    task

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python3
 """jjkkll"""
-from collections import OrderedDict
-
 from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """llllllll"""
-    def __init__(self):
-        """Illlll"""
+    """llllll"""
+
+    def __init__(self) -> None:
+        """ooooo"""
         super().__init__()
-        self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """Ammmmm"""
+        """kkkkk"""
         if key is None or item is None:
             return
-        if key not in self.cache_data:
-            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                last_key, _ = self.cache_data.popitem(True)
-                print("DISCARD:", last_key)
+        if key in self.cache_data.keys():
+            del self.cache_data[key]
         self.cache_data[key] = item
-        self.cache_data.move_to_end(key, last=True)
+        if len(self.cache_data.keys()) > BaseCaching.MAX_ITEMS:
+            first_key = list(self.cache_data.keys())[-2]
+            print("DISCARD: {}".format(first_key))
+            del self.cache_data[first_key]
 
     def get(self, key):
-        """Rllll"""
-        return self.cache_data.get(key, None)
+        """llll"""
+        if key is None or key not in self.cache_data.keys():
+            return None
